8be1ce86f4e239c9dca8199408a31720efd47564
Developer Experience team at Ignite <hello@ignite.com>
2023-06-26 10:57:41 +0000

    Initialized with Ignite CLI

---
+++ cmd/athenad/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"os"
+
+	"github.com/cosmos/cosmos-sdk/server"
+	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+
+	"github.com/placeholder-dapps/athena/app"
+	"github.com/placeholder-dapps/athena/cmd/athenad/cmd"
+)
+
+func main() {
+	rootCmd, _ := cmd.NewRootCmd()
+	if err := svrcmd.Execute(rootCmd, "", app.DefaultNodeHome); err != nil {
+		switch e := err.(type) {
+		case server.ErrorCode:
+			os.Exit(e.Code)
+
+		default:
+			os.Exit(1)
+		}
+	}
+}
